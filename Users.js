a2189045e62620b5f9400fac8394e5bb777eca2d
mayur8989 <mayur.rupareliya9@yahoo.com>
2017-06-17 18:08:25 +0530

    Add files via upload

---
+++ Users.js
@@ -0,0 +1,109 @@
+﻿
+
+function GetAll() {
+
+    $.ajax({
+        url: "http://localhost:51423/api/Users/GetUsers",
+        type: 'GET',
+        datatype: 'JSON',
+        success: function (data) {
+            tbl(data);
+        },
+        error: function () {
+            alert(Error);
+        }
+    })
+}
+
+function tbl(usr) {
+    var result = '<table><th>Id</th><th>Name</th><th>PhoneNo</th><th>City</th>';
+    $.each(usr, function (index, user) {
+        result += "<tr><td>" + user.Id + "</td><td>" + user.Name + "</td><td>" + user.PhoneNo + "</td><td>" + user.City + "</td><td><button onclick='Getbyid(" + user.Id + ")'>update</button><button onclick='Delete(" + user.Id + ")'>delete</button></td></tr>";
+    })
+    result += "</table>";
+    $("#tblData").html(result);
+}
+
+
+function Insert() {
+    var nm = $("#txtName").val();
+    var pn = $("#txtPhoneNo").val();
+    var ct = $("#txtCity").val();
+
+    $("#btnUpdate").hide();
+    $("#btnInsert").show();
+    var result = {
+        Name: nm,
+        PhoneNo: pn,
+        City: ct
+    }
+
+    $.ajax({
+        type: 'POST',
+        data: JSON.stringify(result),
+        url: 'http://localhost:51423/api/Users/PostUser',
+        contentType: "application/json",
+        success: function () {
+
+        }
+    });
+
+}
+
+function Getbyid(id) {
+    alert(id);
+    $.ajax({
+        type: 'GET',
+        //data: JSON.stringify(result),
+        url: 'http://localhost:51423/api/Users/GetUser/'+id,
+        contentType: "application/json",
+        success: function (data) {
+            console.log(data);
+            $("#btnUpdate").show();
+            $("#btnInsert").hide();
+            $("#txtName").val(data.Name);
+            $("#txtPhoneNo").val(data.PhoneNo);
+            $("#txtCity").val(data.City);
+            $("#userid").val(data.Id);
+        }
+    });
+}
+
+function Update() {
+    var nm = $("#txtName").val();
+    var pn = $("#txtPhoneNo").val();
+    var ct = $("#txtCity").val();
+    var id = $("#userid").val();
+    alert(id);
+    
+    var result = {
+        Id:id,
+        Name: nm,
+        PhoneNo: pn,
+        City: ct
+    }
+
+    $.ajax({
+        type: 'POST',
+        data: JSON.stringify(result),
+        url: 'http://localhost:51423/api/Users/UpdateUser',
+        contentType: "application/json",
+        success: function () {
+
+        }
+    });
+}
+
+function Delete(id) {
+    alert(id);
+    $.ajax({
+        type: 'DELETE',
+        //data: JSON.stringify(result),
+        url: 'http://localhost:51423/api/Users/DeleteUser/' + id,
+        contentType: "application/json",
+        success: function () {
+            alert("ok");
+        }
+    });
+}
+
---
+++ UsersController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Crudoperation_Webapi;
+
+namespace Crudoperation_Webapi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private MvcCrudOperationEntities db = new MvcCrudOperationEntities();
+
+        // GET: api/Users
+        public IEnumerable<User> GetUsers()
+        {
+            return db.Users.ToList();
+        }
+
+        // GET: api/Users/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult GetUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        // PUT: api/Users/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUser(int id, User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Users/InsertUser
+        //[ResponseType(typeof(User))]
+        [HttpPost]
+        public IHttpActionResult PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Users.Add(user);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = user.Id }, user);
+        }
+
+        // DELETE: api/Users/5
+        [ResponseType(typeof(User))]
+        public IHttpActionResult DeleteUser(int id)
+        {
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            db.Users.Remove(user);
+            db.SaveChanges();
+
+            return Ok(user);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        [HttpPost]
+        public IHttpActionResult UpdateUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            //var dataupdate = db.UpdateRecord(user.Id, user.Name, user.PhoneNo, user.City);
+            db.UpdateRecord(user.Id, user.Name, user.PhoneNo, user.City);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = user.Id }, user);
+        }
+
+        private bool UserExists(int id)
+        {
+            return db.Users.Count(e => e.Id == id) > 0;
+        }
+    }
+}
